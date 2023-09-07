a0fb5f5ef895320c39300a56ad4b79dfd033bc0a
mariomemes <116306146+mariomemes@users.noreply.github.com>
2023-09-07 08:28:17 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,49 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+<head>
+<style>
+body {
+background-image: url("your-background-image.jpg");
+background-size: cover;
+background-position: center;
+color: white;
 }
+
+
+.button-container {
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+}
+
+
+.button-container button {
+width: 200px;
+height: 50px;
+font-size: 16px;
+background-color: transparent;
+border: 1px solid white;
+color: lime;
+opacity: 0.7;
+/* Adjust the opacity value as needed */
+}
+
+
+.button-container button:hover {
+opacity: 1;
+}
+</style>
+</head>
+
+
+<body>
+<div class="button-container">
+<button onclick="window.location.href='https://main--genuine-hotteok-158c57.netlify.app/games/singlehtmlgames/terraria'" style="width: 200px; height: 50px; font-size: 16px;">Terraria</button>
+<button onclick="window.location.href='https://main--gleeful-crepe-0a5b53.netlify.app/'" style="width: 200px; height: 50px; font-size: 16px;">Mountain</button>
+<button onclick="window.location.href='https://xtendo.onrender.com/'" style="width: 200px; height: 50px; font-size: 16px;">Proxy</button>
+<button onclick="window.location.href='https://rel2sp2.windows96.net/'" style="width: 200px; height: 50px; font-size: 16px;">Windows96</button>
+<button onclick="window.location.href='https://retroslash.netlify.app/'" style="width: 200px; height: 50px; font-size: 16px;">RetroOS</button>
+</div>
